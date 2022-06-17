7c161d54c8c809989596045284e6f82b831cb0dc
Nyoman Trimakna <99968326+Nyomantrimakna@users.noreply.github.com>
2022-06-17 11:14:28 +0800

    menambahkan page home dan about

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react'
+// import {Routes, Route} from 'react-router-dom';
+// import Home from './content/home/home';
+// import About from './content/about/about';
+// import Fakultas from './content/fakultas/Fakultas';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// const App = () => {
+// 	return(
+// 		<Routes>
+// 			<Route path='/' element={<Home />}/>
+// 			<Route path='RightWay' element={<About />} />
+// 			<Route path='fakultas' element={<Fakultas />} />
+// 		</Routes>
+// 	) 
+// }
+
+// export default App;
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const App = () => {
+	const [data, setData] = useState([]);
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState(false);
+
+	useEffect(() => {
+		async function fetchData() {
+			setLoading(true);
+			setError(false);
+			try {
+				const result = await axios.get('http://universities.hipolabs.com/search?country=United+States');
+				setData(result.data);
+			}
+			catch (err) {
+				setError(true);
+			}
+			setLoading(false);
+		}
+		fetchData();
+	}, []);
+
+	return (
+		<div>
+			{loading && <p>Loading...</p>}
+			{error && <p>Error</p>}
+			{data.map((item, index) => (
+				<div key={index}>
+					<h3>{item.name}</h3>
+				</div>
+			))}
+		</div>
+	)
 }
 
 export default App;
