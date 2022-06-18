24eb550c64dd0b3f0ed8e58fa4bf536131a26511
Nyoman Trimakna <99968326+Nyomantrimakna@users.noreply.github.com>
2022-06-18 09:58:50 +0800

    menambahkan home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,56 +1,17 @@
-// import React from 'react'
-// import {Routes, Route} from 'react-router-dom';
-// import Home from './content/home/home';
-// import About from './content/about/about';
-// import Fakultas from './content/fakultas/Fakultas';
-
-// const App = () => {
-// 	return(
-// 		<Routes>
-// 			<Route path='/' element={<Home />}/>
-// 			<Route path='RightWay' element={<About />} />
-// 			<Route path='fakultas' element={<Fakultas />} />
-// 		</Routes>
-// 	) 
-// }
-
-// export default App;
-
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react'
+import {Routes, Route} from 'react-router-dom';
+import Home from './content/home/home';
+import About from './content/about/about';
+import Fakultas from './content/fakultas/Fakultas';
 
 const App = () => {
-	const [data, setData] = useState([]);
-	const [loading, setLoading] = useState(false);
-	const [error, setError] = useState(false);
-
-	useEffect(() => {
-		async function fetchData() {
-			setLoading(true);
-			setError(false);
-			try {
-				const result = await axios.get('http://universities.hipolabs.com/search?country=United+States');
-				setData(result.data);
-			}
-			catch (err) {
-				setError(true);
-			}
-			setLoading(false);
-		}
-		fetchData();
-	}, []);
-
-	return (
-		<div>
-			{loading && <p>Loading...</p>}
-			{error && <p>Error</p>}
-			{data.map((item, index) => (
-				<div key={index}>
-					<h3>{item.name}</h3>
-				</div>
-			))}
-		</div>
-	)
+	return(
+		<Routes>
+			<Route path='/' element={<Home />}/>
+			<Route path='RightWay' element={<About />} />
+			<Route path='fakultas' element={<Fakultas />} />
+		</Routes>
+	) 
 }
 
 export default App;
